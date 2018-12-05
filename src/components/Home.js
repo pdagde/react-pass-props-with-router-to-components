32753fbf80288e29652f48e1aef8a2f854442571
pdagde <pravin.dagde@knorex.com>
2018-12-05 15:00:46 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+import Image from '../img.png'
+class Home extends Component{
+    state = {
+        posts:[ ]
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(res =>{
+            console.log("results are ",res)
+            this.setState({
+                posts : res.data
+            })
+        } )
+    }
+    render(){
+        const postsList = this.state.posts.length  ? (
+            this.state.posts.map(post=>{
+                return(
+                    <div className="post card" key={post.id}>
+                    <div className="card-content yellow">
+                    <img src={Image} alt='collour combination'></img>
+
+                        <Link to={'/'+ post.id}>
+                             <span className="card-title"> {post.title}</span>
+                        </Link>
+                        <p>{post.body}</p>
+                    </div>
+                  </div>
+                )
+            })
+        ) :(
+                <div>
+                    posts are not found
+                </div>
+            
+        )
+        return(
+            <div className="container home">
+                <h4 className="center">Home</h4>
+                <div>{postsList}</div>
+            </div>
+        )
+    }
+}
+export default Home
